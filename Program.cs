90e96296427c107fc703d19ab7db1bc73b7b0b6d
Maria Aramayo <marialujanaramayo@gmail.com>
2022-07-15 18:22:38 -0300

    clase usuario y producto sin terminar

--- Program.cs
+++ Program.cs
@@ -1,5 +1,22 @@
 ﻿namespace main
 {
-
+    public class Usuario
+    {
+        private int usuarioId;
+        private string nombre;
+        private string apellido;
+        private string nombreUsuario;
+        private string contrasenia;
+        private string mail;
+    }
+    public class Producto
+    {
+        private int productoId;
+        private string descripcion;
+        private double costo;
+        private double precioVenta;
+        private int stock;
+        private int usuarioId;
+    }
 }
 
